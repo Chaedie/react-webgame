be1e0abe730f200ee688d6bf2cedfa9de42b977d
Chaedie <chaedong.im.dev@gmail.com>
2022-10-22 16:56:48 +0900

    Add: functional component로 변경

--- 2-끝말잇기/WordRelay.jsx
+++ 2-끝말잇기/WordRelay.jsx
@@ -1,45 +1,40 @@
+const { useState } = require('react');
 const React = require('react');
-const { Component } = React;
 
-class WordRelay extends Component {
-  state = {
-    text: '시작',
-    input: '',
-    result: '',
-    score: 0,
-  };
+function WordRelay() {
+  const [text, setText] = useState('시작');
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const [score, setScore] = useState(0);
 
-  onSubmitInput = e => {
+  const onSubmitInput = e => {
     e.preventDefault();
-    const { text, input, score } = this.state;
     if (text[text.length - 1] === input[0]) {
-      this.setState({ result: '딩동댕' });
-      this.setState({ input: '' });
-      this.setState({ text: input });
-      this.setState({ score: score + 1 });
+      setResult('딩동댕');
+      setInput('');
+      setText(input);
+      setScore(prev => prev + 1);
     } else {
-      this.setState({ result: '땡' });
+      setResult('땡');
     }
   };
 
-  render() {
-    return (
-      <>
-        <header>
-          <h1>구구단</h1>
-        </header>
-        <main>
-          <h3>제시어 : {this.state.text}</h3>
-          <form onSubmit={this.onSubmitInput}>
-            <input type="text" value={this.state.input} onChange={e => this.setState({ input: e.target.value })} />
-            <input type="submit" value="submit" />
-          </form>
-          <h3>결과 : {this.state.result}</h3>
-          <h3>점수 : {this.state.score}</h3>
-        </main>
-      </>
-    );
-  }
+  return (
+    <>
+      <header>
+        <h1>구구단</h1>
+      </header>
+      <main>
+        <h3>제시어 : {text}</h3>
+        <form onSubmit={onSubmitInput}>
+          <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+          <input type="submit" value="submit" />
+        </form>
+        <h3>결과 : {result}</h3>
+        <h3>점수 : {score}</h3>
+      </main>
+    </>
+  );
 }
 
 module.exports = WordRelay;
