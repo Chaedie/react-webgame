6367d69ac035197e1f3921288ad6755df0e61068
Chaedie <chaedong.im.dev@gmail.com>
2022-10-24 12:27:16 +0900

    Learn: 리렌더링 제어 (shouldComponentUpdate) 학습

--- 3-숫자야구/Try.jsx
+++ 3-숫자야구/Try.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 function Try({ tryInfo }) {
   const { id, input, result } = tryInfo;
 
+  const [changedResult, setChangedResult] = useState(result);
+
   return (
     <li>
-      {id}번째 결과 : "input: {input}" {result}
+      <p>
+        {id}번째 결과 : "input: {input}" {result}
+      </p>
+      <p onClick={() => setChangedResult(1)}>{changedResult}여기클릭</p>
     </li>
   );
 }
 
-export default Try;
+// export default Try;
+
+// React.memo로 래핑하면 props가 같을 때 상위 컴포넌트 리렌더링에 의해
+// 억울하게 자식도 리렌더링 되는 걸 막을 수 있다.
+export default React.memo(Try);
--- 3-숫자야구/client.jsx
+++ 3-숫자야구/client.jsx
@@ -1,5 +1,11 @@
 const React = require('react');
 const ReactDom = require('react-dom/client');
 const NumberBaseball = require('./NumberBaseball');
+const { default: Test } = require('./RenderTest');
 
-ReactDom.createRoot(document.querySelector('#root')).render(<NumberBaseball />);
+ReactDom.createRoot(document.querySelector('#root')).render(
+  <>
+    <NumberBaseball />
+    <Test />
+  </>
+);
