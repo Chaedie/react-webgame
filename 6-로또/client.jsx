21800c544bb5394f8c3542fcd04f0621042ce254
Chaedie <chaedong.im.dev@gmail.com>
2022-11-30 10:49:59 +0900

    Feat: 6-로또 클래스 컴포넌트 구현

---
+++ 6-로또/client.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LottoClass from './LottoClass';
+import LottoHooks from './LottoHooks';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <>
+    <LottoClass />
+    <LottoHooks />
+  </>
+);
