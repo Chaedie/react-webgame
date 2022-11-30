d9e3ec81d04e23d519a8fd866f13f67d03cfbdb3
Chaedie <chaedong.im.dev@gmail.com>
2022-11-30 11:09:55 +0900

    Feat: 6-로또 훅스로 변경

--- 6-로또/LottoClass.jsx
+++ 6-로또/LottoClass.jsx
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 import Ball from './Ball';
-
-function getWinNumbers() {
-  console.log('getWinNumbers');
-  const candidate = Array(45)
-    .fill()
-    .map((_, i) => i + 1);
-  const shuffle = [];
-  while (candidate.length > 0) {
-    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
-  }
-
-  const bonusNumber = shuffle[shuffle.length - 1];
-  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
-  return [...winNumbers, bonusNumber];
-}
+import { getWinNumbers } from './getWinNumbers';
 
 class LottoClass extends Component {
   state = {
@@ -33,13 +19,13 @@ class LottoClass extends Component {
         this.setState(prevState => {
           return { winBalls: [...prevState.winBalls, winNumbers[i]] };
         });
-      }, (i + 1) * 1000);
+      }, (i + 1) * 100);
       this.timeouts[6] = setTimeout(() => {
         this.setState({
           bonus: winNumbers[6],
           redo: true,
         });
-      }, 7000);
+      }, 700);
     }
   };
   componentDidMount() {
