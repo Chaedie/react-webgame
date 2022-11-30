dc83096dbf85ea27005b655f42231dc40d9869a3
Chaedie <chaedong.im.dev@gmail.com>
2022-11-30 20:28:32 +0900

    Feat: useReducer 사용하여 틱택토 클릭 구현

--- 7-틱택토/TicTacToe.jsx
+++ 7-틱택토/TicTacToe.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useCallback, useReducer } from 'react';
 import Table from './Table';
 
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+};
+
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    case CLICK_CELL:
+      const tableData = [...state.tableData];
+      tableData[action.rowIndex] = [...tableData[action.rowIndex]];
+      tableData[action.rowIndex][action.cellIndex] = state.turn;
+      return {
+        ...state,
+        tableData,
+      };
+    case CHANGE_TURN:
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O',
+      };
+
+    default:
+      break;
+  }
+};
+
 function TicTacToe() {
-  const [winner, setWinner] = useState('');
-  const [turn, setTurn] = useState('O');
-  const [tableData, setTableData] = useState(['', '', ''], ['', '', ''], ['', '', '']);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState(['', '', ''], ['', '', ''], ['', '', '']);
+
+  const onClickTable = useCallback(() => {
+    dispatch({ type: 'SET_WINNER', winner: 'O' });
+  }, []);
+
+  // 이 객체가 액션 객체이며, 디스패치가 액션객체를 실행한다.
+  // 액션을 해석해서 스테이트를 바꿔주는 것이 리듀셔이다.
 
   return (
     <>
-      <Table></Table>
-      {winner && <div>틱택토</div>}
+      <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}></Table>
+      {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
 }
